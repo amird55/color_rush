32468b7aaeda99dc4ca9f141b61a36c200295dcd
amird55 <amir.dgn@outlook.com>
2025-08-21 19:17:01 +0300

    הצגת ריבוע ראשי של תצוגה

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import MainDisplay from "./MainDisplay.jsx";
 
 function App() {
 
   return (
     <>
+      <MainDisplay />
     </>
   )
 }
---
+++ src/MainDisplay.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function MainDisplay(props) {
+    return (
+        <div className="bigDiv" style={{}}></div>
+    );
+}
+
+export default MainDisplay;
